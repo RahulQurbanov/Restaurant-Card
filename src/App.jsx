a4544324e61e587e70c38d74f5e58256e8a476c6
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2025-05-18 18:47:50 +0400

    Cheking

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,34 @@ import RestaurantCard from "./components/RestaurantCard";
 export default function App() {
   const [restaruant] = useState([
     {
-    name: "Şirvan Restoranı",
+    name: "Casa Mexico",
     imageUrl: "https://c4.wallpaperflare.com/wallpaper/570/745/92/comida-mexico-plato-tacos-wallpaper-preview.jpg",
     rating: 4.5,
-    cuisine: "Azərbaycan mətbəxi",
-    },
-    {
-      name: "Italiano Pizza",
-      imageUrl: "https://www.shutterstock.com/shutterstock/videos/1105246327/thumb/1.jpg?ip=x480",
-      rating: 4.2,
-      cuisine: "İtalyan mətbəxi",
+    cuisine: "Meksika mətbəxi",
     },
     {
       name: "Sushi World",
-      imageUrl: "https://i.pinimg.com/1200x/4f/1c/f0/4f1cf0a91fdee088f1d0ca9284181252.jpg",
+      imageUrl: "https://c4.wallpaperflare.com/wallpaper/282/207/746/comida-cuenco-japon-pescado-wallpaper-preview.jpg",
       rating: 4.7,
       cuisine: "Yapon mətbəxi",
+    },
+    {
+      name: "Italiano Pizza",
+      imageUrl: "https://c4.wallpaperflare.com/wallpaper/545/786/565/pizza-4k-image-wallpaper-preview.jpg",
+      rating: 4.9,
+      cuisine: "İtalyan mətbəxi",
+    },
+    {
+      name: "Burger Town",
+      imageUrl: "https://i.pinimg.com/736x/de/46/ef/de46ef982efb3131b4dee36516e74764.jpg",
+      rating: 4.2,
+      cuisine: "Amerika mətbəxi",
+    },
+    {
+      name: "Tandoori Palace",
+      imageUrl: "https://c4.wallpaperflare.com/wallpaper/959/866/476/food-rice-biryani-mutton-wallpaper-preview.jpg",
+      rating: 4.6,
+      cuisine: "Hindistan mətbəxi",
     },
   ]);
   return (
