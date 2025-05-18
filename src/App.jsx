15e48e7a5190ac0035a01b9e0ea132dc946d0d06
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2025-05-18 18:25:28 +0400

    Add Restaurants Data

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ export default function App() {
   const [restaruant] = useState([
     {
     name: "Şirvan Restoranı",
-    imageUrl: "https://source.unsplash.com/300x200/?restaurant",
+    imageUrl: "https://c4.wallpaperflare.com/wallpaper/570/745/92/comida-mexico-plato-tacos-wallpaper-preview.jpg",
     rating: 4.5,
     cuisine: "Azərbaycan mətbəxi",
-    },{
+    },
+    {
       name: "Italiano Pizza",
-      imageUrl: "https://source.unsplash.com/300x200/?pizza",
+      imageUrl: "https://www.shutterstock.com/shutterstock/videos/1105246327/thumb/1.jpg?ip=x480",
       rating: 4.2,
       cuisine: "İtalyan mətbəxi",
     },
     {
       name: "Sushi World",
-      imageUrl: "https://source.unsplash.com/300x200/?sushi",
+      imageUrl: "https://i.pinimg.com/1200x/4f/1c/f0/4f1cf0a91fdee088f1d0ca9284181252.jpg",
       rating: 4.7,
       cuisine: "Yapon mətbəxi",
     },
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -2,7 +2,7 @@ import "./card.css";
 export default function RestaurantCard({name,cuisine,rating,imageUrl}) {
     return (
         <div className="card">
-          <img src={imageUrl} alt={name} className="card-image" />
+          <div className="image"><img src={imageUrl} alt={name} className="card-image" /></div>
           <div className="card-body">
             <h2 className="card-title">{name}</h2>
             <p className="card-cuisine">{cuisine}</p>
